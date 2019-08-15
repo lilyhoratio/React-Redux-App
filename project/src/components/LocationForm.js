139b18e5e86e2ad5502e7c83a26599afc50e5716
lilyhoratio <lilyhzhou@gmail.com>
2019-08-15 15:31:30 -0700

    wrote out action functions

--- project/src/components/LocationForm.js
+++ project/src/components/LocationForm.js
@@ -1,30 +1,32 @@
 import React from 'react'
+import { getData } from '../actions'
 
 const LocationForm = (props) => {
     console.log("form props", props)
 
     return (
-        <>
-            <form>
-                <label>Enter latitude:
-                <input
-                        name="latitude"
-                        type="text"
-                        placeholder="37.8267"
-                    // onChange={handleChange}
-                    />
-                </label>
-                <label>Enter longitude:
-            <input
-                        name="longitude"
-                        type="text"
-                        placeholder="-122.4233"
-                    // onChange={handleChange}
-                    />
-                </label>
-                <button onClick={props.getData}>Get Weather</button>
-            </form>
-        </>
+        // <>
+        //     <form>
+        //         <label>Enter latitude:
+        //         <input
+        //                 name="latitude"
+        //                 type="text"
+        //                 placeholder="37.8267"
+        //             // onChange={handleChange}
+        //             />
+        //         </label>
+        //         <label>Enter longitude:
+        //     <input
+        //                 name="longitude"
+        //                 type="text"
+        //                 placeholder="-122.4233"
+        //             // onChange={handleChange}
+        //             />
+        //         </label>
+        //         <button onClick={props.getData}>Get Weather</button>
+        //     </form>
+        // </>
+        <button onClick={props.getData}>Get Weather</button>
     )
 }
 
--- project/src/reducers/weatherReducer.js
+++ project/src/reducers/weatherReducer.js
@@ -1,3 +1,9 @@
+import {
+    FETCH_WEATHER_DATA_START,
+    FETCH_WEATHER_DATA_SUCCESS,
+    FETCH_WEATHER_DATA_FAILURE,
+} from '../actions'
+
 const initialState = {
     weatherData: {
 
@@ -10,6 +16,19 @@ const initialState = {
 export const reducer = (state = initialState, action) => {
     // on initial load, redux calls reducer once with random string for action.type
     switch (action.type) {
+        case FETCH_WEATHER_DATA_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            }
+        case FETCH_WEATHER_DATA_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                weatherData: action.payload,
+                error: ''
+            };
 
         default:
             return state;
